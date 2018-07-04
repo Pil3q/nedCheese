3a252194b3cbbec1525b24a9cda12ace3b791ca8
Patryk Pilecki <ppilecki@icloud.com>
2018-07-04 15:55:17 +0100

    variable naming - currency name to currency source

--- cheeseCounter.js
+++ cheeseCounter.js
@@ -4,8 +4,8 @@ fetch(url).then(response => {
   return response.json();
 }).then(data => {
   if (window.location.search === "?country=uk") {
-      priceSetter(data.rates.EUR, data.rates.GBP, 19.99, '£')
-    }
+    priceSetter(data.rates.EUR, data.rates.GBP, 19.99, '£')
+  }
   else if (window.location.search === '?country=japan') {
     priceSetter(data.rates.EUR, data.rates.JPY, 3000.00, '¥')
   }
@@ -14,10 +14,10 @@ fetch(url).then(response => {
   document.getElementById("pieces").innerHTML = 'god knows how many(something went wrong)'
 });
 function priceSetter(baseCurrency, currency, price, symbol) {
-  let euro = 1 / baseCurrency
-  let gbp = 1 / currency
-  let euroGbp = euro/gbp
-  let cheesePieces = Math.floor(price/(3.24 * euroGbp))
+  let base = 1 / baseCurrency
+  let customerCurrency = 1 / currency
+  let euroCustomer = euro/customerCurrency
+  let cheesePieces = Math.floor(price/(3.24 * euroCustomer))
   document.getElementById("pieces").innerHTML = cheesePieces + ` blocks of cheese for ${symbol}${price}`
   document.getElementById("cheese").innerHTML = '&#129472;'.repeat(cheesePieces)
 }
